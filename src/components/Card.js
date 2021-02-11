8d120f2cc666901be277a319224cb1c0a85c23a4
Nightrider0098 <hitesh08011999@gmail.com>
2021-02-11 13:07:18 +0530

    update the hidden states

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react'
 
 export default function Card(props) {
-    const [hiddenState, setHiddenState] = useState(localStorage.getItem(props.lan + props.idx + 'd') ? true : false)
-    return (
 
-        <div style={hiddenState ? { display: none } : {}}>
-            <div>
+    const [hiddenState, setHiddenState] = useState((localStorage.getItem(props.lan + props.idx + 'd') === 'true') ? true : false)
+    const [likes, setLikes] = useState((localStorage.getItem(props.lan + props.idx + 'l') ? true : false))
+    return (
+        <div >
+            <div style={(hiddenState === true) ? { display: 'none' } : {}}>
+                {hiddenState, "is your data"}
+                {typeof (localStorage.getItem(props.lan + props.idx + 'd'))}
+                {"This is the data"}
                 <div>
-                    {props.data.title}
+                    <div>
+                        {props.data.title}
+                    </div>
+                    <div>
+                        {props.data.source ? props.data.source.name : ""}
+                    </div>
+                    <div>{props.data.publishedAt}</div>
                 </div>
-                <div>
-                    {props.data.source ? props.data.source.name : ""}
-                </div>
-                <div>{props.data.publishedAt}</div>
+
             </div>
+
             <button onClick={() => {
-            }}><span className="like_count">{props.like_count || 0}</span>Like</button>
+                localStorage.setItem(props.lan + props.idx + 'l', !likes)
+                setLikes(!likes)
+            }}><span className="like_count">{likes === true ? 1 : 0}{" "}</span>Like</button>
             <button onClick={() => {
                 localStorage.setItem(props.lan + props.idx + 'd', !hiddenState)
                 setHiddenState(!hiddenState)
             }}> Hide</button>
         </div>
-
     )
 }
