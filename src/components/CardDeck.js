f10f2401faa38c38aa32605f3e386dd00a54e69c
Nightrider0098 <hitesh08011999@gmail.com>
2021-02-15 22:53:13 +0530

    updated the cardDeck structure

--- src/components/CardDeck.js
+++ src/components/CardDeck.js
@@ -5,15 +5,12 @@ export default function CardDeck(props) {
     const [currentPage, setCurrentPage] = useState(1)
     const [currentData, setCurrentData] = useState([])
     const [searchString, setSearchString] = useState('')
-    // const [searchString, setSearchString] = useState('')
-    const [justToggle, setJustToggle] = useState(0);
     const [searchMode, setsearchMode] = useState(false);
     const renderCards = () => {
         if (currentData && currentData.length) {
             let lan = props.lan
             let retArray = []
             if (currentData.length > 0) {
-                // console.log('printing')
                 for (let i = 0; i < 3; i++) {
                     retArray.push(< Card lan={lan} idx={currentPage * 3 + i - 3} searchMode={searchString === '' ? false : true} data={currentData[currentPage * 3 + i - 3]} key={lan + currentPage * 3 + i + 'id'
                     } />)
@@ -24,20 +21,15 @@ export default function CardDeck(props) {
 
 
     useEffect(() => {
-        if (currentPage !== 1) {
 
-            setSearchString("")
-            setCurrentPage(1);
-        }
-        else {
+        setSearchString("")
+        setsearchMode(false)
 
-            setSearchString("")
-            setsearchMode(false)
-            fetch(`https://gnews.io/api/v4/top-headlines?token=410db42779f25b2d81028050efe65502&page=${currentPage}&lang=${props.lan}`).then(e => e.json()).then(data => {
-                setCurrentData(data['articles'])
-            })
+        fetch(`https://gnews.io/api/v4/top-headlines?token=410db42779f25b2d81028050efe65502&page=${1}&lang=${props.lan}`).then(e => e.json()).then(data => {
+            setCurrentData(data['articles'].slice(currentPage * 3 - 3, currentPage * 3 - 1))
+        })
 
-        }
+
     }, [props.lan])
 
     const searchTheBar = () => {
@@ -45,41 +37,48 @@ export default function CardDeck(props) {
         fetch(`https://gnews.io/api/v4/search?q=${searchString}&token=410db42779f25b2d81028050efe65502&page=${1}&lang=${props.lan}`).then(e => e.json()).then(data => {
             setCurrentData(data['articles'])
         });
-        setCurrentData(1);
+        setCurrentPage(1);
     }
-    useEffect(() => {
-
-        if (searchString === '')
-            fetch(`https://gnews.io/api/v4/top-headlines?token=410db42779f25b2d81028050efe65502&page=${currentPage}&lang=${props.lan}`).then(e => e.json()).then(data => {
-                setCurrentData(data['articles'])
-            })
-        else {
-            fetch(`https://gnews.io/api/v4/search?q=${searchString}&token=410db42779f25b2d81028050efe65502&page=${currentPage}&lang=${props.lan}`).then(e => e.json()).then(data => {
-                setCurrentData(data['articles'])
-            })
+    const movePage = (move) => {
+        let currPage = currentPage;
+        if (currentPage + move < 4 && currentPage + move > 0) {
+            setCurrentPage(currentPage + move);
+            if (searchMode) {
+                fetch(`https://gnews.io/api/v4/search?q=${searchString}&token=410db42779f25b2d81028050efe65502&page=${currentPage + move}&lang=${props.lan}`).then(e => e.json()).then(data => {
+                    setCurrentData(data['articles'].slice((currPage - 1) * 3, currPage * 3))
+                })
+            } else {
+                fetch(`https://gnews.io/api/v4/top-headlines?token=410db42779f25b2d81028050efe65502&page=${currentPage + move}&lang=${props.lan}`).then(e => e.json()).then(data => {
+                    setCurrentData(data['articles'].slice((currPage - 1) * 3, currPage * 3))
+                })
+            }
         }
-    }, [currentPage, justToggle])
+
+    }
 
 
     return (
         <div>
             <input id='searchBox' value={searchString} onChange={(e) => { setSearchString(e.target.value) }} onKeyDown={(e) => {
-                if (e.key === 'Enter') { searchTheBar(); }
+                if (e.key === 'Enter') {
+                    searchTheBar();
+                }
             }} />
             {renderCards()}
             <button id='search' onClick={() => {
-                searchTheBar();
+                searchTheBar(1);
             }}>search</button>
-            <button id='Prev' disabled={currentPage == 1 ? true : false} onClick={() => { currentPage > 1 ? setCurrentPage(currentPage - 1) : undefined }}>Prev</button>
-            <button id='Next' disabled={currentPage == 3 ? true : false} onClick={() => { currentPage < 3 ? setCurrentPage(currentPage + 1) : undefined }}>Next</button>
-            <button id='exitSearchMode' style={!searchMode ? { display: 'none' } : {}} onClick={() => {
+            <button id='Prev' disabled={currentPage == 1 ? true : false} onClick={() => movePage(-1)}>Prev</button>
+            <button id='Next' disabled={currentPage == 3 ? true : false} onClick={() => movePage(1)}>Next</button>
+            { searchMode ? <button id='exitSearchMode' onClick={() => {
                 setSearchString('');
                 setsearchMode(false)
-                if (currentPage !== 1)
-                    setCurrentPage(1)
-                else
-                    setJustToggle(justToggle + 1);
-            }}>exitSearch Mode</button>
+                setCurrentPage(1);
+                fetch(`https://gnews.io/api/v4/top-headlines?token=410db42779f25b2d81028050efe65502&page=${1}&lang=${props.lan}`).then(e => e.json()).then(data => {
+                    setCurrentData(data['articles'].slice(0, 3))
+                })
+
+            }}>exitSearch Mode</button> : ''}
         </div >
     )
 }
